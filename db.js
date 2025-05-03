9db200d1deb7b7da42964a9a7c660324ecbca478
Thrisith <tthrisith@gmai.com>
2025-05-03 17:52:51 +0530

    Mongodb Atlas connection

--- db.js
+++ db.js
@@ -13,12 +13,14 @@ async function getDatabase() {
   // }
   // return database;
   mongoose
-    .connect("mongodb://127.0.0.1:27017/library")
+    .connect(
+      "mongodb+srv://Thrisith:Test%40123@cluster0.xbqfxun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
     .then(() => {
       console.log("Database Connected");
     })
-    .catch(() => {
-      console.log("Database Connection Failed");
+    .catch((e) => {
+      console.log("Database Connection Failed", e);
     });
 }
 module.exports = {
